ad3260ca289c7eef7553ce0c6e1077ec13440bb6
BasketMan57070 <107278586+BasketMan57070@users.noreply.github.com>
2022-06-10 22:21:12 +0200

    Add files via upload

---
+++ UE10.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+
+	<header style="background-color: #3EC1B0;">
+		<h1>DCG</h1>
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="#">Accueil</a></li>
+			<li><a href="#">Menu déroulant ?</a></li>
+		</ul>
+	</nav>
+
+	<div id="page">
+		<div id="accueil">
+			<a href="UE1.php" class="unite">UE1 : « Fondamentaux du droit »</a>
+			<a href="UE2.php" class="unite">UE2 : « Droit des sociétés »</a>
+			<a href="UE3.php" class="unite">UE3 : « Droit social »</a>
+			<a href="UE4.php" class="unite">UE4 : « Droit fiscal »</a>
+			<a href="UE5.php" class="unite">UE5 : « Économie contemporaine »</a>
+			<a href="UE6.php" class="unite">UE6 : « Finance »</a>
+			<a href="UE7.php" class="unite">UE7 : « Management »</a>
+			<a href="UE8.php" class="unite">UE8 : « Système d’information et de gestion »</a>
+			<a href="UE9.php" class="unite">UE9 : « Comptabilité »</a>
+			<a href="UE10.php" class="unite">UE10 : « Comptabilité approfondie »</a>
+			<a href="UE11.php" class="unite">UE11 : « Contrôle de gestion »</a>
+			<a href="UE12.php" class="unite">UE12 : « Anglais appliqué aux affaires »</a>
+			<a href="UE13.php" class="unite">UE13 : « Relations professionnelles »</a>
+			<a href="UE14.php" class="unite">UE14 : « Épreuve facultative de langue vivante »</a>
+		</div>
+	</div>
+
+	<footer>
+		
+	</footer>
+
+</body>
+
+</html>
